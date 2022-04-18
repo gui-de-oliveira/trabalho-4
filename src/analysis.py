75be892f6a30a8af8f1f2e425a4eb2b2dded50d0
GuiOliveira98 <guil.oliveira98@gmail.com>
2022-04-18 01:21:47 -0300

    Format file

--- src/analysis.py
+++ src/analysis.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -24,6 +24,7 @@ def question2():
     answerNoise = 0.0
     return answerDiscount, answerNoise
 
+
 def question3a():
     # get the close reward, risking the cliff
     answerDiscount = None
@@ -31,6 +32,7 @@ def question3a():
     answerLivingReward = None
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3b():
     # get the close reward, avoiding the cliff
@@ -40,6 +42,7 @@ def question3b():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3c():
     # Prefer the distant exit (+10), risking the cliff (-10)
     answerDiscount = None
@@ -47,6 +50,7 @@ def question3c():
     answerLivingReward = None
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3d():
     # Prefer the distant exit (+10), avoiding the cliff (-10)
@@ -56,6 +60,7 @@ def question3d():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3e():
     # Avoid both exits and the cliff (so an episode should never terminate)
     answerDiscount = None
@@ -64,11 +69,13 @@ def question3e():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question6():
     answerEpsilon = None
     answerLearningRate = None
     return answerEpsilon, answerLearningRate
     # If not possible, return 'NOT POSSIBLE'
+
 
 if __name__ == '__main__':
     print('Answers to analysis questions:')
