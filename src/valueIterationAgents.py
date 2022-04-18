d55b6d2ebc4f34e4da5c4cbe814d455e9e68ccae
GuiOliveira98 <guil.oliveira98@gmail.com>
2022-04-18 01:13:31 -0300

    Implemented ValueIterationAgent (q1)

--- src/valueIterationAgents.py
+++ src/valueIterationAgents.py
@@ -4,16 +4,16 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
-import mdp, util
+import util
 from learningAgents import ValueEstimationAgent
+
 
 class ValueIterationAgent(ValueEstimationAgent):
     """
@@ -24,6 +24,7 @@ class ValueIterationAgent(ValueEstimationAgent):
         for a given number of iterations using the supplied
         discount factor.
     """
+
     def __init__(self, mdp, discount=0.9, iterations=100):
         """
           Your value iteration agent should take an mdp on
@@ -42,9 +43,38 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.iterations = iterations
         self.values = util.Counter()  # A Counter is a dict with default 0
 
-        # Write value iteration code here
-        "*** YOUR CODE HERE ***"
+        for _ in range(iterations):
+            self.update_state_values()
 
+    def update_state_values(self):
+        updated_values = self.values.copy()
+
+        for state in self.mdp.getStates():
+            if self.mdp.isTerminal(state):
+                continue
+
+            highest_action = {"action": None, "value": -999.0}
+
+            for action in self.mdp.getPossibleActions(state):
+                transitionsAndProbs = self.mdp.getTransitionStatesAndProbs(
+                    state, action)
+
+                value = 0.0
+                for transition, prob in transitionsAndProbs:
+                    value += prob * self.values[transition]
+
+                if value > highest_action["value"]:
+                    highest_action = {"action": action, "value": value}
+
+            updated_values[state] = self.getReward(state) + \
+                self.discount * highest_action["value"]
+
+        self.values = updated_values
+
+    # I dont know why action and nextState are needed to calculate the reward
+    # So I made this function to simplify without breaking compatibility
+    def getReward(self, state):
+        return self.mdp.getReward(state, None, "")
 
     def getValue(self, state):
         """
@@ -57,8 +87,15 @@ class ValueIterationAgent(ValueEstimationAgent):
           Compute the Q-value of action in state from the
           value function stored in self.values.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        qvalue = 0.0
+
+        for nextState, prob in self.mdp.getTransitionStatesAndProbs(
+                state, action):
+            next_state_value = self.values[nextState]
+            qvalue += prob * next_state_value
+
+        return self.getReward(state) + self.discount * qvalue
 
     def computeActionFromValues(self, state):
         """
@@ -69,8 +106,26 @@ class ValueIterationAgent(ValueEstimationAgent):
           there are no legal actions, which is the case at the
           terminal state, you should return None.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        possibleActions = self.mdp.getPossibleActions(state)
+
+        #  if there are no legal actions, which is the case at the terminal state, you should return None.
+        if len(possibleActions) == 0:
+            return None
+
+        # The policy is the best action in the given state according to the values currently stored in self.values.
+        highest_value_action = None
+        highest_value = None
+
+        for action in possibleActions:
+            value = self.computeQValueFromValues(state, action)
+
+            if highest_value_action == None or value > highest_value:
+                highest_value_action = action
+                highest_value = value
+                continue
+
+        return highest_value_action
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
